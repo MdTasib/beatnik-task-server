bb4bc7b04a427ab0c33e02fb97b8c789e904af01
MdTasib <ohidulalam442@gmail.com>
2022-11-17 20:47:29 +0600

    database connected

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const { connectToServer } = require("./utils/dbConnected");
+const errorHandler = require("./middlewares/errorHandler");
+const app = express();
+
+// MIDDLEWARES
+app.use(cors());
+app.use(express.json());
+app.use(errorHandler);
+
+// DATABASE CONNECTED
+connectToServer(err => {
+	if (!err) {
+		app.listen(process.env.PORT, () => console.log("Port 5000 server running"));
+	} else {
+		console.log("DB DON'T CONNECTED");
+	}
+});
+
+// NOT FOUND ROUTE
+app.all("*", (req, res) => {
+	res.send("Route Not Found");
+});
+
+// ERROR HANDLE
+process.on("unhandledRejection", error => {
+	console.log(error.name, error.message);
+	app.close(() => {
+		process.exit(1);
+	});
+});
